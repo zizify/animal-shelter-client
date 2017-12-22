505e1c9074333661795ea7279fa15f4a1402bf5d
Andy Gaines <andyagaines@gmail.com>
2017-12-22 16:48:11 -0600

    created cat and dog reducers

--- src/reducers/cat-reducer.js
+++ src/reducers/cat-reducer.js
@@ -6,6 +6,52 @@ const initialState = {
 	data: null
 };
 
-export const catReducer = (state=initialState, catActions) => {
+export const catReducer = (state=initialState, action) => {
+	if (action.type === catActions.FETCH_CAT_SUCCESS) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			data: action.cat
+		});
+	}
+
+	else if (action.type === catActions.FETCH_CAT_ERROR) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: action.error,
+		});
+	}
+
+	else if (action.type === catActions.FETCH_CAT_REQUEST) {
+		return Object.assign({}, state, {
+			loading: true,
+			error: null,
+			data: null
+		});
+	}
+
+	else if (action.type === catActions.ADOPT_CAT_SUCCESS) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			data: action.cat
+		});
+	}
+
+	else if (action.type === catActions.ADOPT_CAT_ERROR) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: action.error,
+		});
+	}
+
+	else if (action.type === catActions.ADOPT_CAT_REQUEST) {
+		return Object.assign({}, state, {
+			loading: true,
+			error: null,
+			data: null
+		});
+	}
+
 	return state;
 };
--- src/reducers/dog-reducer.js
+++ src/reducers/dog-reducer.js
@@ -6,6 +6,52 @@ const initialState = {
 	data: null
 };
 
-export const dogReducer = (state=initialState, dogActions) => {
+export const dogReducer = (state=initialState, action) => {
+	if (action.type === dogActions.FETCH_DOG_SUCCESS) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			data: action.dog
+		});
+	}
+
+	else if (action.type === dogActions.FETCH_DOG_ERROR) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: action.error,
+		});
+	}
+
+	else if (action.type === dogActions.FETCH_DOG_REQUEST) {
+		return Object.assign({}, state, {
+			loading: true,
+			error: null,
+			data: null
+		});
+	}
+
+	else if (action.type === dogActions.ADOPT_DOG_SUCCESS) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			data: action.dog
+		});
+	}
+
+	else if (action.type === dogActions.ADOPT_DOG_ERROR) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: action.error,
+		});
+	}
+
+	else if (action.type === dogActions.ADOPT_DOG_REQUEST) {
+		return Object.assign({}, state, {
+			loading: true,
+			error: null,
+			data: null
+		});
+	}
+
 	return state;
 };
